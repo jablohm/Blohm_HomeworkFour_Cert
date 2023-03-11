d2f3e75541db50de9b505cc4812f858adabf0aac
jablohm <77591463+jablohm@users.noreply.github.com>
2023-03-11 18:17:52 -0500

    ready1

---
+++ app/app.js
@@ -0,0 +1,46 @@
+function changeRoute() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+    // console.log(hashTag + " " + pageID); 
+    $.get(`pages/${pageID}/${pageID}.html`, function(data){
+        console.log("data" + data);
+        $("#app").html(data);
+    });
+}
+
+function initURLlisteners() {
+    $(window).on("hashchange", changeRoute);
+    changeRoute();
+}
+
+$(".btn").click(function(){
+    alert("An action was performed.");
+  });
+  $(".view").click(function(){
+    alert("An action was performed.");
+  });
+
+
+// function initListeners () {
+//     $(".bars").click(function(e) {
+//         // console.log("clicked");
+//         $(".bars").toggleClass("active");
+//         $(".links").toggleClass("active");
+//         // turns added class on and off
+//     });
+
+//     $(".links a").click(function(e) {
+//         // console.log("clicked");
+//         $(".bars").toggleClass("active");
+//         $(".links").toggleClass("active");
+        
+
+//     });
+
+    
+// }
+
+$(document).ready(function() {
+    initListeners();
+    initURLlisteners();
+     });
